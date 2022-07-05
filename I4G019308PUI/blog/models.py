38dd93ae96d52692b7d6e91afd0724a0110ab0b8
Retdyel <77490836+Retdyel@users.noreply.github.com>
2022-07-05 07:57:10 +0100

    Update models.py

--- I4G019308PUI/blog/models.py
+++ I4G019308PUI/blog/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 class Post(models.Model):
     Title = models.CharField(max_length=200)
-    Text = models.TextField
+    Text = models.TextField()
     Author = get_user_model()
-    Created_date = models.DateTimeField
-    Published_date = models.DateTimeField
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
     
